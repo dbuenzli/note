5c5c1887b77839376e3ce1d6b02592b56c082711
Daniel Bünzli <daniel.buenzli@erratique.ch>
2023-07-29 16:16:17 +0200

    Spdxify.

--- src/brr/note_brr_legacy.ml
+++ src/brr/note_brr_legacy.ml
@@ -1,6 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2018 The brr programmers. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
+   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
 
@@ -112,19 +112,3 @@ module Store = struct
     if Jv.is_none (Window.to_jv G.window) then E.never else
     (Evr.on_target storage (fun _ -> ()) (Window.as_target G.window))
 end
-
-(*---------------------------------------------------------------------------
-   Copyright (c) 2018 The brr programmers
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  ---------------------------------------------------------------------------*)
--- src/note.ml
+++ src/note.ml
@@ -1,6 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2018 The note programmers. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
+   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
 module rec Src : sig (* Sources where data is pushed *)
@@ -944,19 +944,3 @@ module S = struct
 
   let dump_src_ids = C.dump_src_ids
 end
-
-(*---------------------------------------------------------------------------
-   Copyright (c) 2018 The note programmers
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  ---------------------------------------------------------------------------*)
